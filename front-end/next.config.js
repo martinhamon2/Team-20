05ff6799f0bd66a7105dd8da5e26d22a8936cfbb
Arnovda1 <arno@van-der-auwera.com>
2026-05-08 17:29:39 +0200

    added x-frame-options deny for frontend

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,6 +1,17 @@
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+        ],
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
